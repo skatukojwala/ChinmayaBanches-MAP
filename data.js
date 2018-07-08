2996262d7deb4b6ed0fd907cae4e18d4772d4b92
SRINIVAS KATUKOJWALA <skatukoj@us.ibm.com>
2018-07-08 15:25:51 -0700

    Cleanup and more features

---
+++ data.js
@@ -0,0 +1,115 @@
+var data = 
+[
+ {
+   "Name": "CENTRAL CHINMAYA MISSION TRUST - MAHARASHTRA",
+   "Address": "Saki Vihar Road, Powai, Mumbai, Maharashtra 400072, India",
+   "Lat": 19.1324891,
+   "Long": 72.8985255,
+   "Country": "India"
+ },
+ {
+   "Name": "CHINMAYA AWADH - LUCKNOW - UTTAR PRADESH",
+   "Address": "C-4, A, H. Park, Mahanagar Extension, Lucknow U.P.,Lucknow - 226006, Uttar Pradesh, India",
+   "Lat": 26.883565,
+   "Long": 80.954263,
+   "Country": "India"
+ },
+ {
+   "Name": "CHINMAYA BALANIVASA - ANDHRA PRADESH",
+   "Address": "S.V.N Colony, Guntur - 522 006, Andhra Pradesh, India",
+   "Lat": 16.325842,
+   "Long": 80.415104,
+   "Country": "India"
+ },
+ {
+   "Name": "CHINMAYA HARIDWAR - OREGON",
+   "Address": "3551 NW John Olsen Pl, Hillsboro, OR 97124",
+   "Lat": 45.546386,
+   "Long": -122.880986,
+   "Country": "USA"
+ },
+ {
+   "Name": "CHINMAYA INTERNATIONAL FOUNDATION (CIF) - KERALA",
+   "Address": "Adi Sankara Nilayam, Veliyanad, Ernakulam District, Kerala, India - 682319",
+   "Lat": 9.879144,
+   "Long": 76.455565,
+   "Country": "India"
+ },
+ {
+   "Name": "CHINMAYA MISSION VIJAYANAGARAM - ANDHRA PRADESH",
+   "Address": "Vizianagaram Cantonment, Vizianagaram, Andhra Pradesh 535003, India",
+   "Lat": 18.111636,
+   "Long": 83.395244,
+   "Country": "India"
+ },
+ {
+   "Name": "CHINMAYA MISSION ADONI - ANDHRA PRADESH",
+   "Address": "2/158 - A, Chinmaya Cultural Complex, Raghavendra Nagar, Adoni Arts College Road - 518302",
+   "Lat": 15.630349,
+   "Long": 77.290614,
+   "Country": "India"
+ },
+ {
+   "Name": "CHINMAYA MISSION AHMEDABAD - GUJARAT",
+   "Address": "Param Dham Satellite Road, Jodhpur Tekra, Ahmedabad - 380 015",
+   "Lat": 23.028391,
+   "Long": 72.520227,
+   "Country": "India"
+ },
+ {
+   "Name": "CHINMAYA MISSION AHMEDNAGAR - MAHARASHTRA",
+   "Address": "No.6 Old Prant Kacheri, Markendeya Housing Society, Aurangabad Road, Ahmednagar - 414 001",
+   "Lat": 19.835939,
+   "Long": 75.292161,
+   "Country": "India"
+ },
+ {
+   "Name": "CHINMAYA MISSION AKIVEDU - ANDHRA PRADESH",
+   "Address": "Tank Bund, Kodanda Ramalayam Street, West Godavari District, Akkiveedu- 534 235, Andhra Pradesh",
+   "Lat": 16.582544,
+   "Long": 81.373271,
+   "Country": "India"
+ },
+ {
+   "Name": "CHINMAYA MISSION NEW YORK",
+   "Address": "26 E 91st St, New York, NY 10128",
+   "Lat": 40.784172,
+   "Long": -73.956988,
+   "Country": "USA"
+ },
+ {
+   "Name": "CHINMAYA JYOTI - SAN DIEGO",
+   "Address": "12249 9th St, San Diego, CA 920648",
+   "Lat": 32.958662, 
+   "Long": -117.065740,
+   "Country": "USA"
+ },
+ {
+   "Name": "CHINMAYA MISSION ORLANDO",
+   "Address": "1221 Chinmaya Way, Casselberry, FL 32707",
+   "Lat": 28.668592, 
+   "Long": -81.292174,
+   "Country": "USA"
+ },
+ {
+   "Name": "CHINMAYA MISSION SINGAPORE",
+   "Address": "67 Tessensohn Rd, Singapore 217667",
+   "Lat": 1.315442, 
+   "Long": 103.856435,
+   "Country": "Singapore"
+ },
+ {
+   "Name": "Chinmaya Mission Melbourne",
+   "Address": "4 Pioneer Dr, Templestowe VIC 3106, Australia",
+   "Lat": -37.768284, 
+   "Long": 145.147453,
+   "Country": "Australia"
+ },
+ {
+   "Name": "CHINMAYA MISSION SYDNEY - NEW SOUTH WALES",
+   "Address": "38 Carrington Rd, Castle Hill NSW 2154, Australia",
+   "Lat": -33.728927, 
+   "Long": 150.985325,
+   "Country": "Australia"
+ }
+]
